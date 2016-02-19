f4f6be245cbd7b055b128b1d6bd141218d369f86
James <jammatpic@gmail.com>
2016-02-20 02:12:26 +1100

    Added title, sounds, and more JS comments

--- resources/js/pomodoro-timer.js
+++ resources/js/pomodoro-timer.js
@@ -1,11 +1,13 @@
-// SHOULD ADD CIRCULAR PROGRESS BAR AND SOUNDS
-// ADD COMMENTS
-var clockActive = false; // if clock is in middle of countdown
+var clockActive = false; // indicates if clock is in middle of countdown or not
 var countSecs; // amount of seconds timer is set to, in total
-var timerSecs; // amount of seconds in timer
+var timerSecs; // amount of seconds in timer currently
 var pauseTime = -1; // amount of time left if paused
-var sessionDone = false; // if session has just been completed
+var sessionDone = false; // indicates if session has just been completed
+// sound effects
+var bleep = new Audio("resources/sound/bleep.mp3");
+var beep = new Audio("resources/sound/beep.mp3");
 
+// enables changing length of break and session
 function changeLength(value, buttonType) {
 	displayValue = $(value).text();
 	if (buttonType == "+") {
@@ -19,7 +21,9 @@ function changeLength(value, buttonType) {
 	}
 }
 
+// starts timer counting down
 function timerStart(timerLength, timerType) {
+    // changes start button to a pause button, and displays session/break status
 	document.getElementById("btn-start").setAttribute("class", "btn btn-pause");
 	$("#btn-start").text("Pause");
 	if (timerType == "session") {
@@ -27,8 +31,10 @@ function timerStart(timerLength, timerType) {
 	} else if (timerType == "break") {
 		$("#status").text("Break");
 	}
+    //clears and resets timer to a given length
 	clearInterval(countSecs);
 	var timerMins = Number(timerLength);
+    // gets the right time if previously paused (if pauseTime is -1, the timer hasn't been paused.)
 	if (pauseTime == -1) {
 		timerSecs = timerMins * 60;
 	} else {
@@ -39,49 +45,60 @@ function timerStart(timerLength, timerType) {
 
 	countSecs = setInterval(function() { 
 		timerSecs = Number(timerSecs) - 1;
+        // displays text of timer
 		if (timerSecs % 60 >= 10) {
 			$("#time").text(Math.floor(timerSecs / 60) + ":" + timerSecs % 60);
 		} else {
 			$("#time").text(Math.floor(timerSecs / 60) + ":0" + timerSecs % 60);
 		}
+        // if timer reaches zero...
 		if (timerSecs == 0) {
 			timerSecs = timerMins * 60;
 			clockActive = false;
 			clearInterval(countSecs);
+            // if last timer was a session, start a break
 			if (timerType == "session") {
 				$("#time").text($("#break-length").text() + ":" + "00");
 				sessionDone = true;
+                beep.play();
 				timerStart($("#break-length").text(), "break");
+            // if last timer was a break, start a session
 			} else if (timerType == "break") {
 				$("#time").text($("#session-length").text() + ":" + "00");
 				sessionDone = false;
+                bleep.play();
 				timerStart($("#session-length").text(), "session");
 			}
 		}
-	}, 1000);
+	}, 1);
 }
 
+// initialises timer once button is pressed
 function timer() {
 	var sessionLength = $("#session-length").text();
 	var breakLength = $("#break-length").text();
-	if (clockActive == true) { //for pausing
+    // pauses timer if currently counting down
+	if (clockActive == true) { 
 		pauseTime = timerSecs;
 		clearInterval(countSecs);
 		clockActive = false;
 		document.getElementById("btn-start").setAttribute("class", "btn btn-normal");
 		$("#btn-start").text("Start");
+    // starts timer if not currently counting down
 	} else {
 		if (sessionDone == false) { // if last timer was a break
-			timerStart(sessionLength, "session");
+			timerStart(sessionLength, "session"); // start a session
 		} else { // if last timer was a session
-			timerStart(breakLength, "break");
+			timerStart(breakLength, "break"); // start a break
 		}
 	}
 }
 
 $(document).ready(function() {
+    // when changing length of break
     $(".break-change").on("click", function() {
-        if (clockActive != true && sessionDone == true) { // if clock is not active, reset all timers with new info
+        // if clock is not counting down, reset timer from start with new break length
+        if (clockActive != true && sessionDone == true) {
             changeLength("#break-length", $(this).text());
             $("#time").text($("#break-length").text() + ":00");
             clearInterval(countSecs);
@@ -91,12 +108,15 @@ $(document).ready(function() {
             document.getElementById("btn-start").setAttribute("class", "btn btn-normal");
             $("#btn-start").text("Start");
             $("#debug").text(pauseTime);
+        // if clock is not counting down, set the length of the next break
         } else {
-            changeLength("#break-length", $(this).text()); // if clock is active, set the next timer
+            changeLength("#break-length", $(this).text()); 
         }
     });
 
+    // when changing length of session
     $(".session-change").on("click", function() {
+        // if clock is not counting down, reset timer from start with new session length
         if (clockActive != true && sessionDone == false) { 
             changeLength("#session-length", $(this).text());
             $("#time").text($("#session-length").text() + ":00");
@@ -107,15 +127,18 @@ $(document).ready(function() {
             document.getElementById("btn-start").setAttribute("class", "btn btn-normal");
             $("#btn-start").text("Start");
             $("#debug").text(pauseTime);
+        // if clock is not counting down, set the length of the next session
         } else {
             changeLength("#session-length", $(this).text()); 
         }
     });
 
+    // starting/pausing the timer
     $("#btn-start").on("click", function() {
         timer();
     });
 
+    // resetting the timer
     $("#btn-reset").on("click", function() {
         $("#time").text($("#session-length").text() + ":00");
         $("#status").text("Session");
